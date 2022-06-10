71f00cb58111c51d663bf607d783b6bb770f2852
SindhuGowda-Amaze <you@example.com>
2022-06-10 12:25:15 +0530

    wsa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Pages/CommonPages/header/header.component';
+import { FooterComponent } from './Pages/CommonPages/footer/footer.component';
+import { SidebarComponent } from './Pages/CommonPages/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
